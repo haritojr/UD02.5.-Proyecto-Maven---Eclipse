9816ea297ad21d84e9d859962e6e94d7f6a972f4
haritojr <93643248+haritojr@users.noreply.github.com>
2021-11-03 10:26:43 +0100

    Add files via upload

---
+++ fibonacciMain/src/main/java/es/iessoterhernandez/daw/endes/fibonacciMain/App.java
@@ -0,0 +1,10 @@
+package es.iessoterhernandez.daw.endes.fibonacciMain;
+
+import es.iessoterhernandez.daw.endes.fibonacci.App;
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	App.fib(4);
+    }
+}
